64f54d5940ef14d565c2494a94c8788fa0774b57
Stephen Lin <linsteph@Stephens-MacBook-Pro-2.local>
2019-08-24 19:48:02 -0400

    Camera working

--- app/src/main/java/com/hackathon/hackathon/MainActivity.java
+++ app/src/main/java/com/hackathon/hackathon/MainActivity.java
@@ -1,25 +1,55 @@
 package com.hackathon.hackathon;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    TextView textView;
+    Button btnCam;
+    private static final int REQUEST_CAPTURE_IMAGE=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCam = (Button)findViewById(R.id.btnCapture);
+        imageView = (ImageView)findViewById(R.id.textureView);
+
+        btnCam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,REQUEST_CAPTURE_IMAGE);
+            }
+
+        });
     }
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-
-    public void takePicture(){
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
     }
 }

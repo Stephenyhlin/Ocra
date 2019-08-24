acf2ce855b0d7fedc3eff0938601d9965f37c7de
Aser <asermalik@live.com>
2019-08-24 17:07:24 -0400

    take picture with intent delegation

--- app/src/main/java/com/hackathon/hackathon/MainActivity.java
+++ app/src/main/java/com/hackathon/hackathon/MainActivity.java
@@ -2,7 +2,9 @@ package com.hackathon.hackathon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public void takePicture(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }

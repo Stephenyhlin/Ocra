9d1535321f579e39631f58dbfed2a74726f3e684
ShauryaMdn <shaurya8990@gmail.com>
2019-08-24 20:30:01 -0400

    Modified upload code

--- app/src/main/java/com/hackathon/hackathon/UploadToS3.java
+++ app/src/main/java/com/hackathon/hackathon/UploadToS3.java
@@ -1,69 +1,35 @@
 package com.hackathon.hackathon;
 
 import com.amazonaws.SdkClientException;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.*;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class UploadToS3 {
 
     public boolean s3Upload() {
-        Regions clientRegion = Regions.DEFAULT_REGION;
+        Regions clientRegion = Regions.US_EAST_2;
         String bucketName = "*** Bucket name ***";
-        String keyName = "*** Key name ***";
-        String filePath = "*** Path to file to upload ***";
-
-        File file = new File(filePath);
-        long contentLength = file.length();
-        long partSize = 512 * 1024; // Set part size to 512 kB.
+        String fileObjKeyName = "*** File object key name ***";
+        String fileName = "*** Path to file to upload ***";
 
         try {
+            //This code expects that you have AWS credentials set up per:
+            // https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/setup-credentials.html
             AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
                     .withRegion(clientRegion)
-                    .withCredentials(new ProfileCredentialsProvider())
                     .build();
 
-            // Create a list of ETag objects. You retrieve ETags for each object part uploaded,
-            // then, after each individual part has been uploaded, pass the list of ETags to
-            // the request to complete the upload.
-            List<PartETag> partETags = new ArrayList<PartETag>();
-
-            // Initiate the multipart upload.
-            InitiateMultipartUploadRequest initRequest = new InitiateMultipartUploadRequest(bucketName, keyName);
-            InitiateMultipartUploadResult initResponse = s3Client.initiateMultipartUpload(initRequest);
-
-            // Upload the file parts.
-            long filePosition = 0;
-            for (int i = 1; filePosition < contentLength; i++) {
-                // Because the last part could be less than 5 MB, adjust the part size as needed.
-                partSize = Math.min(partSize, (contentLength - filePosition));
-
-                // Create the request to upload a part.
-                UploadPartRequest uploadRequest = new UploadPartRequest()
-                        .withBucketName(bucketName)
-                        .withKey(keyName)
-                        .withUploadId(initResponse.getUploadId())
-                        .withPartNumber(i)
-                        .withFileOffset(filePosition)
-                        .withFile(file)
-                        .withPartSize(partSize);
-
-                // Upload the part and add the response's ETag to our list.
-                UploadPartResult uploadResult = s3Client.uploadPart(uploadRequest);
-                partETags.add(uploadResult.getPartETag());
-
-                filePosition += partSize;
-            }
-
-            // Complete the multipart upload.
-            CompleteMultipartUploadRequest compRequest = new CompleteMultipartUploadRequest(bucketName, keyName,
-                    initResponse.getUploadId(), partETags);
-            s3Client.completeMultipartUpload(compRequest);
+            // Upload a file as a new object with ContentType and title specified.
+            PutObjectRequest request = new PutObjectRequest(bucketName, fileObjKeyName, new File(fileName));
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType("plain/text");
+            metadata.addUserMetadata("x-amz-meta-title", "someTitle");
+            request.setMetadata(metadata);
+            s3Client.putObject(request);
             return true;
         } catch (SdkClientException e) {
             // The call was transmitted successfully, but Amazon S3 couldn't process
